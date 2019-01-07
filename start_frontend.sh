de0133a116db11f31e7458a27c9cdf65ef6b36ab
aaronjonesii <jonesaa5@msu.edu>
2019-01-07 00:38:30 -0500

    Update start_frontend.sh

--- start_frontend.sh
+++ start_frontend.sh
@@ -1,3 +1,4 @@
-cd /root/WebApp/frontend/angularSPA2/
+alias proj="cd /root/WebApp/frontend/angularSPA2"
+. proj
 npm i
 nohup ng s --host 93.188.164.182 --disable-host-check >> testing_angular.log &
